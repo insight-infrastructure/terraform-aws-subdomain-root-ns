47807e64c8e68486bb1d11ed0659286f7bf6dc11
robc-io <robc.io.opensource@gmail.com>
2019-09-05 10:40:51 -0700

    adding ns records

--- main.tf
+++ main.tf
@@ -16,8 +16,24 @@ data "aws_route53_zone" "root" {
   name = "${var.root_domain_name}."
 }
 
+
 resource "aws_route53_zone" "subdomain" {
   name = "${var.subdomain}.${var.root_domain_name}."
 
   tags = local.tags
+}
+
+
+resource "aws_route53_record" "subdomain_root_records" {
+  zone_id = data.aws_route53_zone.root.zone_id
+  name = "${var.subdomain}.${var.root_domain_name}"
+  type = "NS"
+  ttl = "30"
+
+  records = [
+    aws_route53_zone.subdomain.name_servers[0],
+    aws_route53_zone.subdomain.name_servers[1],
+    aws_route53_zone.subdomain.name_servers[2],
+    aws_route53_zone.subdomain.name_servers[3],
+  ]
 }
